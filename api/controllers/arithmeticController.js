2db7dd1fe586dcff5af09cda6a8146d745b8dc65
Naveen Patel <naveenpatel@outlook.com>
2020-02-04 04:50:17 -0800

    Testing

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -16,7 +16,7 @@ exports.calculate = function(req, res) {
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
   };
-
+  // Dummy commit
   // Determine the operation
 
   if (! req.query.operation) {
